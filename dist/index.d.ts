cb7c0ffede87cb2009043e4a056fed10f61c6939
AlexSearha <contact.mytechcompanion.fr>
2024-07-06 13:38:20 +0200

    add ts build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,2 @@
 export declare const truncateString: (sentence: string, maxwidth: number) => string;
+export declare const fetchGeneric: (url: string, method?: string, body?: FormData | null) => Promise<any>;
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,36 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.truncateString = void 0;
+exports.fetchGeneric = exports.truncateString = void 0;
 const truncateString = (sentence, maxwidth) => {
     if (sentence.length > maxwidth) {
-        return sentence.split("").splice(0, maxwidth).join("") + "...";
+        return sentence.split('').splice(0, maxwidth).join('') + '...';
     }
     return sentence;
 };
 exports.truncateString = truncateString;
+const fetchGeneric = (url_1, ...args_1) => __awaiter(void 0, [url_1, ...args_1], void 0, function* (url, method = 'GET', body = null) {
+    try {
+        const options = {
+            method: method,
+            headers: {},
+            body: body instanceof FormData ? body : JSON.stringify(body),
+        };
+        const response = yield fetch(url, options);
+        const data = yield response.json();
+        return data;
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+});
+exports.fetchGeneric = fetchGeneric;
