6af8703850683d5e5f756c0622fe957898f9fae0
AlexSearha <contact.mytechcompanion.fr>
2024-07-06 13:37:10 +0200

    add fetchGeneric function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 export const truncateString = (sentence: string, maxwidth: number): string => {
   if (sentence.length > maxwidth) {
-    return sentence.split("").splice(0, maxwidth).join("") + "...";
+    return sentence.split('').splice(0, maxwidth).join('') + '...';
   }
   return sentence;
+};
+
+export const fetchGeneric = async (url: string, method: string = 'GET', body: FormData | null = null) => {
+  try {
+    const options: RequestInit = {
+      method: method,
+      headers: {},
+      body: body instanceof FormData ? body : JSON.stringify(body),
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
 };
